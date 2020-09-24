624b1e11d54ef43e5bfc9d318497054f294a3e44
garrettkmoody <extrillieos@MSI.localdomain>
2020-09-23 18:27:06 -0700

    UI update

---
+++ app/src/main/java/com/example/thebloomapp/menteeInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.thebloomapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menteeInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mentee_info);
+    }
+}
