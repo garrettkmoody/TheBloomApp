26a6ac5fcaaff1b484c93e3091920ec8be17c839
KCore1 <33336327+KCore1@users.noreply.github.com>
2021-01-16 23:09:44 -0800

    Added mentee info page

--- app/src/main/java/com/example/thebloomapp/theMenteeInfoActivity.java
+++ app/src/main/java/com/example/thebloomapp/theMenteeInfoActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -14,6 +17,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,33 +30,49 @@ public class theMenteeInfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_the_mentee_info);
         Intent intent = getIntent();
         uid = intent.getStringExtra("uid");
-        Button button = findViewById(R.id.menteeInfoButton);
-
+        final ImageView profile = findViewById(R.id.menteeProfile);
+        final TextView name = findViewById(R.id.menteeInfoName);
         final FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-
-        button.setOnClickListener(new View.OnClickListener() {
+        DatabaseReference databaseReference = firebaseDatabase.getReference(uid);
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(theMenteeInfoActivity.this, uid, Toast.LENGTH_SHORT).show();
-                final DatabaseReference myRef = firebaseDatabase.getReference(uid);
-                myRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if(snapshot.exists()) {
-                        System.out.println(snapshot.getValue(UserProfile.class).getName()); }
-                    }
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    UserProfile tempProfile = snapshot.getValue(UserProfile.class);
+                    Picasso.get().load(tempProfile.getLink()).fit().centerCrop().transform(new CircleTransform()).into(profile);
+                    name.setText(tempProfile.getName());
+                }
+            }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        Toast.makeText(theMenteeInfoActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-                Map<String, Object> update = new HashMap<>();
-                update.put("name", "Billy Tow");
-                myRef.updateChildren(update);
-                finish();
-                startActivity(new Intent(theMenteeInfoActivity.this, MainActivity.class));
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(theMenteeInfoActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(theMenteeInfoActivity.this, uid, Toast.LENGTH_SHORT).show();
+//                final DatabaseReference myRef = firebaseDatabase.getReference(uid);
+//                myRef.addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                        if(snapshot.exists()) {
+//                        System.out.println(snapshot.getValue(UserProfile.class).getName()); }
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError error) {
+//                        Toast.makeText(theMenteeInfoActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//                Map<String, Object> update = new HashMap<>();
+//                update.put("name", "Billy Tow");
+//                myRef.updateChildren(update);
+//                finish();
+//                startActivity(new Intent(theMenteeInfoActivity.this, MainActivity.class));
+//            }
+//        });
     }
 }
